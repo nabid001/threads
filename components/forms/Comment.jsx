5c512d9a4273a649bd522a5075d6a547f2cedcb6
Nabid <nabid.explore@gmail.com>
2023-11-06 20:48:05 +0600

    comment and replay is complete

---
+++ components/forms/Comment.jsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { Input } from "../ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+import { usePathname, useRouter } from "next/navigation";
+import { CommentValidation } from "@/lib/validations/threads";
+import Image from "next/image";
+import { addCommentToThread } from "@/lib/actions/thread.actions";
+
+const Comment = ({ threadId, currentUserImg, currentUserId }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: "",
+    },
+  });
+
+  const onSubmit = async (values) => {
+    await addCommentToThread(
+      threadId,
+      values.thread,
+      JSON.parse(currentUserId),
+      pathname
+    );
+
+    form.reset();
+  };
+
+  return (
+    <Form {...form}>
+      <form className="comment-form" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex w-full items-center gap-3">
+              <FormLabel>
+                <Image
+                  src={currentUserImg}
+                  alt="profile-img"
+                  width={48}
+                  height={48}
+                  className="rounded-full object-cover"
+                />
+              </FormLabel>
+              <FormControl className="bg-transparent border-none">
+                <Input
+                  type="text"
+                  placeholder="Comment..."
+                  className="no-focus text-light-1 outline-none"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="comment-form_btn">
+          Reply
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default Comment;
